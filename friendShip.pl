652fe61c3b7d06921fe691ed9c5b3f3f9ad8eafc
jobe64 <jonny.bertilsson@gmail.com>
2013-11-20 15:38:58 +0100

    Update friendShip.pl
    Added some usefull variables.

--- friendShip.pl
+++ friendShip.pl
@@ -2,5 +2,8 @@
 use strict;
 use warnings;
 
-my $oldFriend = "Jonny";
+my @oldFriend = ("Jonny", "Jim");
+my @newFriend = ("Bear", "Haze", "Laze");
 my $currentFriendShipStatus = 0;
+my $TCstatus = 50;
+my $nurseFriendShip = 0;
