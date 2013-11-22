6daa6687ba628e3e1127c1b82eff64284eef4369
jobe64 <jonny.bertilsson@gmail.com>
2013-11-22 15:08:43 +0100

    Update friendShip.pl

--- friendShip.pl
+++ friendShip.pl
@@ -7,7 +7,14 @@ my @newFriend = ("Bear", "Haze", "Laze");
 my $currentFriendShipStatus = 0;
 my $TCstatus = 50;
 my $nurseFriendShip = 0;
+my $tellLiesToNurseFriendShip = 0;
+my $tickle = 0;
+my $oldFriendLaughing = 0;
+
 
 for (my $i = $#newFriend; $i > -1; $i--) {
   splice @array, $i, 1;
 }
+
+
+exit $errorStatus;
